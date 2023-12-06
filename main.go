ec9ee3b8303ad7d8cf17539b1552773fee75a2f5
MikhailPANDA <bl365@bk.ru>
2023-12-06 23:00:56 +0300

    ёлочка

--- main.go
+++ main.go
@@ -1,187 +1,37 @@
-// DONE:
-// деление
-// дроби (5.2)
-// удалять все пробелы
-// скобки
-// нет чисел в выражении
-// нет операндов в выражении
-// успешная обработка одиночного числа
-
-// to do!
-// exponents(!)
-// 2-3% - проценты
-// 2%2 - остаток деления
-// битовый оператор (што ето...)
-// использовать struct, чтобы не использовать цепочку конвертаций string-float-string?
-
-// Ошибки:
-// некорректное соотношение кол-ва операторов и чисел
-// некорректная последовательность операндов "---2", "2+2-" "5..2"
-// ошибка на запятую, или замена запятой на точку
-
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	var expr string
-	for i := 1; i < len(os.Args); i++ {
-		expr = expr + os.Args[i]
-	}
-	Calculator(expr)
+	Spruce()
 }
 
-func Calculator(expr string) {
-
-	expr = checksExpr(expr)
-	exprSlice := separator(parseParntheses(expr))
-
-	if len(exprSlice) == 0 {
-		fmt.Println(expr)
-		return
-	}
-
-	exprSlice = multAndDiv(exprSlice)
-	exprSlice = sumAndSub(exprSlice)
-
-	fmt.Println(exprSlice[0])
-}
-
-func checksExpr(expr string) string {
-	exprSl := strings.Split(expr, " ")
-	var exprUpd string
-	for _, elem := range exprSl {
-		exprUpd += elem
-	}
-
-	if exprUpd[0] == '.' ||
-		exprUpd[0] == ',' ||
-		exprUpd[0] == '*' ||
-		exprUpd[0] == '/' ||
-		exprUpd[0] == '+' {
-		fmt.Println("Incorrect element on first position", exprUpd[0])
-		os.Exit(1)
-	}
-	return exprUpd
-}
-
-func parseParntheses(expr string) string {
-	if countOp, countCl := strings.Count(expr, "("), strings.Count(expr, ")"); countOp != countCl {
-		fmt.Println("Error func parseParntheses(): Unequal number of parentheses")
-		os.Exit(1)
-	} else if countOp == 0 {
-		return expr
-	}
-	// также обработать случаи ")(2+2" "(2+)2" "2)+2("
-
-	for strings.Contains(expr, "(") {
-		posCl := strings.IndexByte(expr, ')')
-		posOp := strings.LastIndexByte(expr[:posCl], '(')
-
-		var exprSlicePart []string
-		exprSlicePart = separator(expr[posOp+1 : posCl]) // распарсить выражение между скобок
-		exprSlicePart = multAndDiv(exprSlicePart)
-		exprSlicePart = sumAndSub(exprSlicePart)
-		expr = expr[:posOp] + exprSlicePart[0] + expr[posCl+1:]
-	}
-	return expr
-}
-
-func multAndDiv(exprSlice []string) []string {
-	if len(exprSlice) == 1 {
-		return exprSlice
-	}
-	for i := 1; i < len(exprSlice); i++ {
-		var expSlCopyR []string
-		switch exprSlice[i] {
-		case "*":
-			a, b := cnvToFloat(exprSlice[i-1], exprSlice[i+1])
-			expSlCopyR = exprSlice[i+2:]                           // копируем часть слайса, исключая все пройденные элементы, текущий и следующий
-			exprSlice = exprSlice[:i]                              // перезаписываем целый слайс, только неполной левой его частью (исключая текущий и предыдущий)
-			exprSlice[i-1] = strconv.FormatFloat(a*b, 'g', -1, 64) // перезаписываем использованное число результатом
-			exprSlice = append(exprSlice, expSlCopyR[:]...)        // добавляем правую часть, таким образом, оригинальный срез потерял 2 элемента
-			i -= 2                                                 // уменьшаем счетчик итераций на 2
-		case "/":
-			a, b := cnvToFloat(exprSlice[i-1], exprSlice[i+1])
-			expSlCopyR = exprSlice[i+2:]
-			exprSlice = exprSlice[:i]
-			exprSlice[i-1] = strconv.FormatFloat(a/b, 'g', -1, 64)
-			exprSlice = append(exprSlice, expSlCopyR[:]...)
-			i -= 2
+func Spruce() { //ёлочка
+	height := 0
+	fmt.Scan(&height)
+	indent := height
+	weight := 1
+	fmt.Printf("\u001B[32m")
+	for i := height; i > 0; i-- {
+		indent--
+		for j := indent; j > 0; j-- { //вывести пробелы, в кол-ве равном indent
+			fmt.Printf(" ")
 		}
-	}
-	return exprSlice
-}
-func sumAndSub(exprSlice []string) []string {
-	if len(exprSlice) == 1 {
-		return exprSlice
-	}
-	for i := 1; i < len(exprSlice); i++ {
-		var expSlCopyR []string
-		switch exprSlice[i] {
-		case "+":
-			a, b := cnvToFloat(exprSlice[i-1], exprSlice[i+1])
-			expSlCopyR = exprSlice[i+2:]
-			exprSlice = exprSlice[:i]
-			exprSlice[i-1] = strconv.FormatFloat(a+b, 'g', -1, 64)
-			exprSlice = append(exprSlice, expSlCopyR[:]...)
-			i -= 2
-		case "-":
-			a, b := cnvToFloat(exprSlice[i-1], exprSlice[i+1])
-			expSlCopyR = exprSlice[i+2:]
-			exprSlice = exprSlice[:i]
-			exprSlice[i-1] = strconv.FormatFloat(a-b, 'g', -1, 64)
-			exprSlice = append(exprSlice, expSlCopyR[:]...)
-			i -= 2
+		for j := weight; j > 0; j-- { //вывести диезы, в кол-ве равном weight
+			fmt.Printf("#")
 		}
+		fmt.Println()
+		weight += 2
 	}
-	return exprSlice
-}
-func separator(expr string) []string {
-	exprSlice := make([]string, 0, 15)
-	segmtSt, countNum, countOper := 0, 0, 0
-	for i := 1; i < len(expr); i++ {
-		if expr[i] == '*' ||
-			expr[i] == '/' ||
-			expr[i] == '+' ||
-			expr[i] == '-' {
-			exprSlice = append(exprSlice, expr[segmtSt:i], string(expr[i]))
-			segmtSt = i + 1
-			countOper++
-			countNum++
-			if i+2 < len(expr) { //если это ещё не предпоследняя итерация
-				i++ //пропустить возможный знак отриц. числа
-			}
+	fmt.Printf("\u001B[0m")
+	fmt.Printf("\x1b[30m")
+	heightTr := height / 3
+	indentTr := height - 1
+	for i := heightTr; i > 0; i-- {
+		for j := indentTr; j > 0; j-- { //вывести пробелы, в кол-ве равном indentTr и диез в конце
+			fmt.Printf(" ")
 		}
-		if i+2 == len(expr) { // если это последняя итерация - добавить последнее значение
-			exprSlice = append(exprSlice, expr[segmtSt:i+2])
-			countNum++
-		}
+		fmt.Println("#")
 	}
-	if countNum == 0 {
-		fmt.Println("Incorrect number of numbers received")
-		os.Exit(1)
-	} else if countOper == 0 {
-		fmt.Println("Incorrect number of operators received")
-		os.Exit(1)
-	}
-	return exprSlice
-}
-func cnvToFloat(a, b string) (float64, float64) {
-	l, err := strconv.ParseFloat(a, 64)
-	if err != nil {
-		fmt.Println("Error func cnvToFloat(), 'a'.", err)
-		os.Exit(1)
-	}
-	r, err2 := strconv.ParseFloat(b, 64)
-	if err2 != nil {
-		fmt.Println("Error func cnvToFloat(), 'b'.", err2)
-		os.Exit(1)
-	}
-	return l, r
+	fmt.Printf("\u001B[0m")
 }

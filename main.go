b212d948f24e78cfa51298897ef6cb682fa85990
MikhailPANDA <bl365@bk.ru>
2024-02-18 20:54:38 +0300

    complete task 02-07

--- main.go
+++ main.go
@@ -3,61 +3,56 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
+	"log"
 	"os"
-	"time"
+	"strings"
 )
 
-func logTime() func() {
-	start := time.Now()
-	return func() {
-		end := time.Now().Sub(start)
-		fmt.Println("log:", end)
-	}
-}
-
 func main() {
-	logger := logTime()
-	defer logger()
-
-	file, err := os.Open("in.txt")
+	fileIn, err := os.Open("data/in2.txt")
 	if err != nil {
-		fmt.Println("os.Open('in.txt')", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-	defer file.Close()
+	defer fileIn.Close()
 
-	scan := bufio.NewScanner(file)
-
-	fileOut, err := os.Create("out.txt")
+	fileOut, err := os.Create("data/data_out.txt")
 	writer := bufio.NewWriter(fileOut)
 	if err != nil {
-		fmt.Println("os.Create('out.txt')", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
+	defer fileOut.Close()
 
+	defer func() {
+		if panicErr := recover(); panicErr != nil {
+
+			fileOut, err := os.Open("data/data_out.txt")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer fileOut.Close()
+
+			dataOut := make([]byte, 1000)
+			fileOut.Read(dataOut)
+
+			fmt.Println("here:", string(dataOut))
+		}
+	}()
+
+	scan := bufio.NewScanner(fileIn)
 	stringNum := 0
 	for scan.Scan() {
 		stringNum++
-		_, err := writer.WriteString(fmt.Sprintf("%v. ", stringNum) + scan.Text() + "\n")
-
-		if err != nil {
-			fmt.Println("writer.WriteString", err)
-			os.Exit(1)
+		strArr := strings.Split(scan.Text(), "|")
+		for _, v := range strArr {
+			if v == "" {
+				log.Panic(fmt.Sprintf("parse error: empty field on string %d", stringNum))
+			}
 		}
+
+		_, err := writer.WriteString(fmt.Sprintf("Row: %d\nName: %s\nAddress: %s\nCity: %s\n\n\n", stringNum, strArr[0], strArr[1], strArr[2]))
+		if err != nil {
+			log.Fatal(err)
+		}
+		writer.Flush()
 	}
-
-	writer.Flush()
-
-	fileOutReader, err := os.Open("out.txt")
-	countByte, err := io.ReadAll(fileOutReader)
-	defer fileOutReader.Close()
-
-	if err != nil {
-		fmt.Println("writer.WriteString", err)
-		os.Exit(1)
-	}
-	fmt.Printf("writed: %v strings, %v bytes\n", stringNum, len(countByte))
-	fileOut.Close()
-
 }
